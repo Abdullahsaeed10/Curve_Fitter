010e9e2df0f80ecba2004e5f3fd2d4bca6c096aa
Mohamed N. Gaafar <46086535+mo-gaafar@users.noreply.github.com>
2022-04-19 04:56:47 +0200

    add more classes

--- src/modules/curvefit.py
+++ src/modules/curvefit.py
@@ -1,29 +1,58 @@
 import numpy as np
+from numpy import fft
 
 
 class Signal():
     def __init__(self, signal=[], fsample=0, time=[]) -> None:
         self.signal = signal
         self.fsample = fsample
-        self.time = np.arange(0, len(signal)) / fsample
 
-# how to deal with extrapolation???????????
+        if self.fsample == 0:
+            self.time = time
+        else:
+            self.time = np.arange(0, len(signal)) / fsample
+        pass
+
+    # operator overload
+    def __subtract__(self, other):
+        if self.fsample == other.fsample:
+            return Signal(self.signal - other.signal, self.fsample, self.time)
+
+    # divide signal into chunks
+    def chunk(self, chunk_size):
+        return [self.signal[i:i + chunk_size] for i in range(0, len(self.signal), chunk_size)]
 
 
-class InterpolatedSignal(Signal):
-    def __init__(self, signal=[], fsample=0) -> None:
-        super().__init__(signal, fsample)
-        self.interpolated_signal = self.interpolate(self.signal)
+class Processing():
+    def __init__(self, original=Signal()) -> None:
+        self.original_signal = original
+        self.modified_signal = original
+        pass
+
+    def reset_signal(self):
+        self.modified_signal = self.original_signal
+        pass
 
     def interpolate(self):
         pass
 
-    def generate_error_map(self):
-        # prograss bar
+    def extrapolate(self):
+        pass
 
-        # multithreading
-        # https://stackoverflow.com/questions/2846653/how-can-i-use-threading-in-python
+    def calculate_error(self):
         pass
 
     def draw_error_equation(self):
         pass
+
+
+def update_graph(self):
+    pass
+
+
+def update_error(self):
+    # prograss bar
+
+    # multithreading
+    # https://stackoverflow.com/questions/2846653/how-can-i-use-threading-in-python
+    pass

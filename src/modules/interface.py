326c5a5cd18a316891e746c0e30a0c71931de1e9
Abdullahsaeed10 <14712019100085@stud.cu.edu.eg>
2022-04-28 22:22:09 +0200

    thread

--- src/modules/interface.py
+++ src/modules/interface.py
@@ -94,13 +94,31 @@ def toggle_fit_mode(self, mode):
 
 ######################## TODO: To be checked ########################
 
-def progressBar_update(self):
+def progressBar_update(self,x):
+    if x==1 :
+        for i in range(50):
+                time.sleep(0.01)
+                self.progressBar.setValue(i+1)
+        return self.progressBar.value()
+    elif x==2 :
+        for i in range(50,80):
+                time.sleep(0.01)
+                self.progressBar.setValue(i+1)
+        return self.progressBar.value()
+    elif x==3 :
+        for i in range(80,100):
+                time.sleep(0.1)
+                self.progressBar.setValue(i+1)
+        return self.progressBar.value()
 
-    for i in range(100):
+
+
+def progressBar_end(self):
+    for i in range(50,100):
             time.sleep(0.001)
             self.progressBar.setValue(i+1)
     return self.progressBar.value()
-
+    
 
 def init_plots(self):
     # initializing plot widgets
@@ -166,8 +184,10 @@ def init_connectors(self):
     
     self.error_map_apply_button = self.findChild(QPushButton, "error_map_apply_button")
     self.error_map_apply_button.clicked.connect(
-         lambda: errormap.calculate_error(self))
+         lambda: errormap.error_map(self))
     
+    
+   
     self.x_comboBox.currentIndexChanged.connect(
         lambda: errormap.select_error_x(self, self.x_comboBox.currentText()))
 
